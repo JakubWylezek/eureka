cfee5af8a09949839b51fcbe2db63fb18c8696f4
Jakub Wylezek <jakub.wylezek@wp.pl>
2019-12-16 18:10:14 +0100

    Initial commit

---
+++ src/test/java/pl/wylezek/petclinic/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wylezek.petclinic.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
